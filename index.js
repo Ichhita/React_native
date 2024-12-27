e1ee8884a191b59f17399ea904ea6ee7aa64ed40
Ichhita <124778398+Ichhita@users.noreply.github.com>
2024-12-27 20:26:33 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-import { registerRootComponent } from 'expo';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import SignInScreen from '../screens/signinScreen';
+import SignUpScreen from '../screens/signupScreen';
+import ConfirmEmailScreen from '../screens/confirmemailScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import NewPasswordScreen from '../screens/NewPasswordScreen';
+import HomeScreen from '../screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import App from './App';
+const Stack = createNativeStackNavigator();
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SignIn" component={SignInScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+            <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
